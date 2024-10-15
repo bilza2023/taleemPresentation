73060aca95735b98d884c6e775780608f08ec34b
bilza2023 <bilza2023@gmail.com>
2024-10-15 14:56:01 +0500

    Alhamdullah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
-
-
-<h1>Taleem Presentation</h1>
-
-<br>
-<br>
-<a href="/static">Static Presentation</a>
-<br>
-<br>
-<a href="/player?filename=fbise9math_ch_1_ex_1.1_q_4_n_properties_of_real_numbers">Player</a>
-<br>
-<br>
-<a href="/editor?id=66505334ba9a237f5dd73370">Editor</a>
-<br>
-<br>
-<a href="/diagnose?filename=fbise9math_ch_1_ex_1.1_q_4_n_properties_of_real_numbers">Diagnose</a>
+<script>
+    // No additional script needed
+  </script>
+  
+  <div class="bg-gray-800 text-white min-h-screen p-8">
+    <h1 class="text-3xl font-bold mb-8 border-b-2 border-white pb-2">Taleem Presentation</h1>
+  
+    <ul class="space-y-6 text-lg font-semibold">
+      <li><a href="/static" class="block border-b border-white pb-2">Static Presentation</a></li>
+      <li><a href="/player?filename=fbise9math_ch_1_ex_1.1_q_4_n_properties_of_real_numbers" class="block border-b border-white pb-2">Player</a></li>
+      <li><a href="/playerNS?filename=fbise9math_ch_1_ex_1.1_q_4_n_properties_of_real_numbers" class="block border-b border-white pb-2">Player2</a></li>
+      <li><a href="/editor?id=66505334ba9a237f5dd73370" class="block border-b border-white pb-2">Editor</a></li>
+    </ul>
+  </div>
+  
--- src/routes/playerNS/+page.svelte
+++ src/routes/playerNS/+page.svelte
@@ -1,18 +1,19 @@
+
 <svelte:head>
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css" integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ" crossorigin="anonymous">
 </svelte:head>
 <script>
+
 //@ts-nocheck 
 /** 6-Nov-2023 : If the core data-structure of a software is decided the software is decided.
  24-Aug 2024 amen to that!!
 */
+
 import { toast } from '@zerodevx/svelte-toast';
 import { onMount } from 'svelte';
 import { fade } from 'svelte/transition';
 import {PlayerToolbar} from '$lib';
 import {db} from "../ajax";
-
-import { Avatar } from '@skeletonlabs/skeleton';
 
 import {PresentationObjUrl,PresentationModeUi} from "$lib";
 
@@ -21,6 +22,8 @@ let presentationObj;
 let pulse = 0;
 
 let ready=false;
+let showToolbarBool = false;
+let interval;
 ////////////////////////////////////////////////////////
 onMount(async ()=>{  
   filename = new URLSearchParams(location.search).get("filename");
@@ -39,15 +42,40 @@ onMount(async ()=>{
     toast.push("failed to load");
  } 
 });
-
+////////////////////////////////////////////
+function start(){
+    interval = setInterval(gameloop , 500);
+}
+function gameloop(){
+   pulse = Math.floor(presentationObj.pulse());
+}
+function stop(){
+  clearInterval(interval);
+}
+function setPulse(value){
+  presentationObj.setPulse(value);
+}
+////////////////////////////////////////////
+function showToolbar(){
+  if (showToolbarBool == false){
+    showToolbarBool = true;  
+    setTimeout(() => {
+    showToolbarBool = false;
+  }, 5000);
+  } 
+}
 </script> 
-{#if presentationObj}
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class='bg-gray-800 text-white w-full min-h-screen' 
+style='position: fixed; top: 0;' on:mousemove={showToolbar} >
 
-      <div class='bg-gray-800 text-white w-full min-h-screen' 
-      style='position: fixed; top: 0;'  >
-      {presentationObj.slides.length}  
-      </div><!--page wrapper-->
-
+      {#if showToolbarBool && presentationObj}
+<PlayerToolbar {presentationObj} {pulse} 
+preStart={start} preStop={stop}  setPulse={setPulse} />
       {/if}
+<div >
+    <PresentationModeUi {presentationObj} {pulse} currentTime={pulse} />
+</div> 
+</div><!--page wrapper-->
 
-<Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-32" rounded="rounded-full"  />
+
